059953feabfc40e4c17590c6bf305160408a419c
Sean Kelman <seanrkelman@gmail.com>
2023-08-21 14:56:31 -0700

    Solved 238 Product of Array Except Self

---
+++ stringsAndArrays/productExcluding.py
@@ -0,0 +1,16 @@
+'''
+Runtime: 82.34%
+Memory Usage: 38.92%
+'''
+from typing import List
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        l = len(nums)
+        products = [1] * l
+        for i in range(1, l):
+            products[i] = products[i-1]* nums[i-1]
+        right = nums[-1]
+        for i in range(l-2, -1, -1):
+            products[i] *= right
+            right *= nums[i]
+        return products
