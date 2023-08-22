baa31a208dbbee4bc635b7279afdea40105dd42e
Sean Kelman <seanrkelman@gmail.com>
2023-08-21 20:03:16 -0700

    Solved 283 Move Zeroes

---
+++ twoPointers/moveZeroes.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        s = 0
+        for f in range(len(nums)):
+            if nums[f] != 0 and nums[s] == 0:
+                nums[s], nums[f] = nums[f], nums[s]
+            if nums[s] != 0:
+                s += 1
