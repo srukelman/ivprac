7fe00bc73f3039234a0bfff37b923b55d183b638
Sean Kelman <seanrkelman@gmail.com>
2023-08-23 11:06:27 -0700

    Solved 207 Course Schedule

---
+++ topoAndHeapSort/courseSchedule.py
@@ -0,0 +1,33 @@
+'''
+Runtime: beats 88.00%
+Memory Usage: beats 86.54%
+'''
+from collections import deque
+from typing import List
+class Solution:
+    def canFinish(self, n: int, prerequisites: List[List[int]]) -> bool:
+        adj = [[] for _ in range(n)]
+        indegree = [0] * n
+        ans = []
+
+        for pair in prerequisites:
+            course = pair[0]
+            prerequisite = pair[1]
+            adj[prerequisite].append(course)
+            indegree[course] += 1
+
+        queue = deque()
+        for i in range(n):
+            if indegree[i] == 0:
+                queue.append(i)
+
+        while queue:
+            current = queue.popleft()
+            ans.append(current)
+
+            for next_course in adj[current]:
+                indegree[next_course] -= 1
+                if indegree[next_course] == 0:
+                    queue.append(next_course)
+
+        return len(ans) == n
