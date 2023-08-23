e23fb82f6da49d050cb6e85c938ac9283657c74b
Sean Kelman <seanrkelman@gmail.com>
2023-08-23 08:46:52 -0700

    Solved 216 Combination Sum III

---
+++ backtracking/combinationSum.py
@@ -0,0 +1,16 @@
+'''
+Runtime: beats 53.64%
+Memory Usage: beats 13.31%
+'''
+from typing import List
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        def dfs(nums, k, n, path, ans):
+            if k < 0 or n < 0: return
+            if k == 0 and n == 0: ans.append(path)
+
+            for i in range(len(nums)):
+                dfs(nums[i + 1:], k-1 , n - nums[i], path + [nums[i]], ans)
+        ans = []
+        dfs(list(range(1,10)), k, n, [], ans)
+        return ans
