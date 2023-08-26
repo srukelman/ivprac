577d8c9f340869dff8cbadf04209b55ed8a38d77
Sean Kelman <seanrkelman@gmail.com>
2023-08-26 13:07:53 -0700

    Solved 332 Reconstruct Itinerary

---
+++ graphs/reconstructItinerary.py
@@ -0,0 +1,29 @@
+'''
+Runtime: beats 97.68%
+Memory Usage: beats 96.80%
+'''
+from typing import List
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        graph = dict()
+        for src, dst in tickets:
+            if src in graph:
+                graph[src].append(dst)
+            else:
+                graph[src] = [dst]
+        for src in graph.keys():
+            graph[src].sort(reverse=True)
+
+        stack = []
+        res = []
+        stack.append("JFK")
+
+        while len(stack) > 0:
+            elem = stack[-1]
+            if elem in graph and len(graph[elem]) > 0:
+                stack.append(graph[elem].pop())
+            else:
+                res.append(stack.pop())
+
+        return res[::-1]
+        
